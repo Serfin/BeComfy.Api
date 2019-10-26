e5fe4318cb7839f70b6031f2eb48d2951424a613
s3rfin <serfin009@gmail.com>
2019-10-27 00:08:18 +0200

    Fixed RESTClient Flight model

--- src/BeComfy.Api/Models/Flights/Flight.cs
+++ src/BeComfy.Api/Models/Flights/Flight.cs
@@ -6,17 +6,14 @@ namespace BeComfy.Api.Models.Flights
 {
     public class Flight
     {
-        public Guid Id { get; private set; }
-        public Guid PlaneId { get; private set; }
-        public IDictionary<SeatClass, int> AvailableSeats { get; private set; }
-        public Guid StartAirport { get; private set; }
-        public IEnumerable<Guid> TransferAirports { get; private set; }
-        public Guid EndAirport { get; private set; }
-        public FlightType FlightType { get; private set; }
-        public decimal Price { get; private set; }
-        public DateTime FlightDate { get; private set; }
-        public DateTime? ReturnDate { get; private set; }
-        public DateTime CreatedAt { get; private set; }
-        public DateTime UpdatedAt { get; private set; }
+        public Guid Id { get; set; }
+        public IDictionary<SeatClass, int> AvailableSeats { get; set; }
+        public Guid StartAirport { get; set; }
+        public IEnumerable<Guid> TransferAirports { get; set; }
+        public Guid EndAirport { get; set; }
+        public FlightType FlightType { get; set; }
+        public decimal Price { get; set; }
+        public DateTime FlightDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
     }
 }
