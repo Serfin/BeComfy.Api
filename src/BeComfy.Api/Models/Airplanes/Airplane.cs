446fea241aba9d633e18ef132cdbd8687787a7de
serfin <serfin009@gmail.com>
2020-02-23 17:35:48 +0100

    Added registraionNumber field

--- src/BeComfy.Api/Models/Airplanes/Airplane.cs
+++ src/BeComfy.Api/Models/Airplanes/Airplane.cs
@@ -7,6 +7,7 @@ namespace BeComfy.Api.Models.Airplanes
         public Guid Id { get; set; }
         public string Model { get; set; }
         public string AirplaneStatus { get; set; }
+        public string AirplaneRegistrationNumber { get; set; }
         public DateTime? NextFlight { get; set; }
         public DateTime? FlightEnd { get; set; }
     }
