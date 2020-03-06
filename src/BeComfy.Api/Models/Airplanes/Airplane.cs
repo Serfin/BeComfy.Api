ec2ceb327fae1799511d9c96a7c55c553ede2fc6
serfin <serfin009@gmail.com>
2020-03-06 22:29:28 +0100

    Added missing mapping

--- src/BeComfy.Api/Models/Airplanes/Airplane.cs
+++ src/BeComfy.Api/Models/Airplanes/Airplane.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using BeComfy.Common.Types.Enums;
 
 namespace BeComfy.Api.Models.Airplanes
 {
@@ -8,7 +10,8 @@ namespace BeComfy.Api.Models.Airplanes
         public string Model { get; set; }
         public string AirplaneStatus { get; set; }
         public string AirplaneRegistrationNumber { get; set; }
+        public IDictionary<SeatClass, int> AvailableSeats { get; set; }
         public DateTime? NextFlight { get; set; }
         public DateTime? FlightEnd { get; set; }
-    }
+    } 
 }
